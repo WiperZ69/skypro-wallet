3deca98d629f8439589de1c631774b790e25b7fd
WiperZ69 <yura.4etin.03@gmail.com>
2025-08-17 00:46:17 +0800

    some changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,28 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import { AnalyticsPage } from "../pages/AnalyticsPage";
-import { ExpensesPage } from "../pages/ExpensesPage";
-import { LogOutPage } from "../pages/LogOutPage";
-import { NotFoundPage } from "../pages/NotFoundPage";
-import SpendingPage from "../pages/SpendingPage";
-import SignInPage from "../pages/SignInPage";
-import SignUpPage from "../pages/SignUpPage";
-import PrivateRoute from "./PrivateRoute";
+import { Route, Routes } from 'react-router-dom'
+import { ExpensesPage } from '../pages/ExpensesPage'
+import { LogOutPage } from '../pages/LogOutPage'
+import { NotFoundPage } from '../pages/NotFoundPage'
+import SignInPage from '../pages/SignInPage'
+import SignUpPage from '../pages/SignUpPage'
+import SpendingPage from '../pages/SpendingPage'
+import PrivateRoute from './PrivateRoute'
 
 function AppRoutes() {
-  return (
-    <Routes>
-      <Route element={<PrivateRoute />}>
-        <Route path="/" element={<ExpensesPage />}>
-          <Route path="/analytic" element={<AnalyticsPage />} />
-          <Route path="/logout" element={<LogOutPage />} />
-        </Route>
-        <Route path="/spending" element={<SpendingPage />} />
-      </Route>
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<PrivateRoute />}>
+				<Route path='/' element={<ExpensesPage />}>
+					<Route path='/logout' element={<LogOutPage />} />
+				</Route>
+				<Route path='/spending' element={<SpendingPage />} />
+			</Route>
+			<Route path='/sign-in' element={<SignInPage />} />
+			<Route path='/sign-up' element={<SignUpPage />} />
+			<Route path='*' element={<NotFoundPage />} />
+		</Routes>
+	)
 }
 
-export default AppRoutes;
+export default AppRoutes
